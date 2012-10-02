e55dcd1ca35c330d243317955e90e44e70836651
maplesod <daniel.mapleson@tgac.ac.uk>
2012-10-02 11:02:48 +0100

    Assembler script: Added command line argument to control memory and verbose output.  Also made better use of constants throughout.

--- scripts/assembler.pl
+++ scripts/assembler.pl
@@ -19,6 +19,10 @@ $DEF_KMER_MAX = 95;
 # Threads constants
 $DEF_THREADS=8;
 
+# Mem constants
+$DEF_MEM=60;
+$MIN_MEM=5;
+
 # Other constants
 $QUOTE = "\"";
 $USAGE="\nassembler [-help] [-p project_name] [-kmin val] [-kmax val] [-a assembler] [-t threads] -i input_file -o output_dir -- script to run an assembly program with multiple k-mer settings with alternate 4 and 6 step increments.  Requests 60GB RAM per assembly.\n\n" . 
@@ -29,6 +33,7 @@ $USAGE="\nassembler [-help] [-p project_name] [-kmin val] [-kmax val] [-a assemb
 "   -kmin  minimum k-mer value in run.  Constraints: " . $KMER_MIN . " <= kmin <= " . $KMER_MAX . "; Last digit must be a '1' or a '5'; Must be <= -kmax. (default = " . $DEF_KMER_MIN . ")\n" .
 "   -kmax  maximum k-mer value in run. Constraints: " . $KMER_MIN . " <= kmax <= " . $KMER_MAX . "; Last digit must be a '1' or a '5'; Must be >= -kmin. (default = " . $DEF_KMER_MAX . ")\n" .
 "   -t     number of threads (default = " . $DEF_THREADS . ")\n" .
+"   -m     memory usage in GB (default = " . $DEF_MEM . "GB)\n" .
 "   -i     list of input files to assemble\n" .
 "   -o     output directory\n\n";
 
@@ -45,9 +50,12 @@ $project_name = "AssemblerMultiKmer_" . $year . $mon . $mday . "_" . $hr . $min 
 $in_files;
 $out_dir;
 $help = 0;
-$kmin = 41;
-$kmax = 95;
-$threads = 8;
+$kmin = $DEF_KMER_MIN;
+$kmax = $DEF_KMER_MAX;
+$threads = $DEF_THREADS;
+$mem = $DEF_MEM;
+$verbose = 0;
+
 
 
 
@@ -58,8 +66,10 @@ $result = GetOptions (	"a=s"    => \$assembler,
 			"kmin=i" => \$kmin,
 			"kmax=i" => \$kmax,
 			"t=i"    => \$threads,
+			"m=i"    => \$mem,
 			"i=s"    => \$in_files,
 			"o=s"    => \$out_dir,
+			"v"      => \$verbose,
 			"help"   => \$help );
 
 
@@ -77,7 +87,8 @@ if ($help) {
 die "Error: No input files specified\n\n" . $USAGE unless $in_files;
 die "Error: No output directory specified\n\n" . $USAGE unless $out_dir;
 
-die "Error: K-mer limits must be >= than 11nt\n\n" . $USAGE unless ($kmin >= 11 && $kmax >= 11);
+die "Error: K-mer limits must be >= " . $KMER_MIN . "nt\n\n" . $USAGE unless ($kmin >= $KMER_MIN && $kmax >= $KMER_MIN);
+die "Error: K-mer limits must be <= " . $KMER_MAX . "nt\n\n" . $USAGE unless ($kmin <= $KMER_MAX && $kmax <= $KMER_MAX);
 die "Error: Min K-mer value must be <= Max K-mer value\n\n" . $USAGE unless ($kmin <= $kmax);
 
 die "Error: K-mer min and K-mer max both must end with a '1' or a '5'.  e.g. 41 or 95.\n\n" . $USAGE unless (validKmer($kmin) && validKmer($kmax));
@@ -86,11 +97,12 @@ die "Error: Invalid assembler requested.  Known assemblers are: 'abyss'.\n\n" . 
 
 die "Error: Invalid number of cores requested.  Must request at least 1 core per assembly.\n\n" .$USAGE unless ($threads >= 1);
 
-
+die "Error: Invalid memory setting.  Must request at least " . $MIN_MEM . "GB.\n\n" .$USAGE unless ($mem >= $MIN_MEM);
 
 # Record current working directory.  The cwd gets restored after the job loop has completed.
 
 $pwd = getcwd;
+$mem_mb = $mem * 1000;
 
 
 # Assembly job loop
@@ -106,17 +118,18 @@ for($i=$kmin; $i<=$kmax;) {
 	$project_arg = "-P " . $project_name;
 	$queue_arg = "-q production";
 	$openmpi_arg = "-a openmpi";
-	$rusage_arg = "-R rusage[mem=60000] space[ptile=8]";
+	$rusage_arg = "-R rusage[mem=" . $mem_mb . "] space[ptile=" . $abyss_threads . "]";
 	$threads_arg = "-n 8";
 	$bsub_args= $job_arg . " " . $project_arg . " " . $queue_arg . " " . $open_mpi_arg . " " . $rusage_arg . " " . $threads_arg;
 	
-	if ($assembler eq "abyss") {
+	if ($assembler eq $A_ABYSS) {
 		$abyss_bin = "abyss-pe";
-		$abyss_core_args = "n=10 np=8 mpirun=mpirun.lsf";
+		$abyss_core_args = "n=10 mpirun=mpirun.lsf";
+		$abyss_threads = "np=" . $threads;
 		$abyss_kmer = "k=" . $i;
 		$abyss_name = "name=Abyss-mpi-k" . $i;
 		$abyss_in = "in='" . $in_files . "'";
-		$cmd = $abyss_bin . " " . $abyss_core_args . " " . $abyss_kmer . " " . $abyss_name . " " . $abyss_in;
+		$cmd = $abyss_bin . " " . $abyss_threads . " " . $abyss_core_args . " " . $abyss_kmer . " " . $abyss_name . " " . $abyss_in;
 	}
 	else {
 		die "Error: Invalid assembler requested.  Also, the script should not have got this far!!!.\n\n" . $USAGE;
@@ -125,7 +138,9 @@ for($i=$kmin; $i<=$kmax;) {
 	system("mkdir", $i_dir);
   	chdir $i_dir;
 
-	print "Executing on cluster: " . $bsub . " " . $bsub_args . " " . $QUOTE . $cmd . $QUOTE . "\n\n";
+	if ($verbose) {
+		print "Executing on cluster: " . $bsub . " " . $bsub_args . " " . $QUOTE . $cmd . $QUOTE . "\n\n";
+	}
 
 	system($bsub, $job_arg, $project_arg, $queue_arg, $openmpi_arg, $rusage_arg, $threads_arg, $cmd);
 
