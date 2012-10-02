95c59c399afb1b2f9f704f5eb32bbf50e3bdb365
maplesod <daniel.mapleson@tgac.ac.uk>
2012-10-02 11:24:26 +0100

    Added the sequence info binary which I took from ~pachorip/clc/clc-ngs-cell-3.0.0beta2-linux_64/sequence_info.
    Also modified the build.sh file which copies all resources necessary to make RAMPART run to the bin directory.

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Directory structure
-SCRIPT_DIR = "./scripts"
-TOOLS_DIR = "./tools"
-TEMPLATE_DIR = "./templates"
-DIST_DIR = "./dist"
+SCRIPT_DIR="./scripts"
+TOOLS_DIR="./tools"
+TEMPLATE_DIR="./templates"
+BIN_DIR="./bin"
 
 # Script / Tool file names
-ARTIFACT = "./artifact.sh"
-ASSEMBLER = $SCRIPT_DIR"/assembler.pl"
-SCAFFOLDER = $SCRIPT_DIR"/scaffolder.pl"
-GAPCLOSER = $SCRIPT_DIR"/gapcloser.pl"
-REPORTER = $TOOLS_DIR"/reporter/dist/reporter.jar"
+ARTIFACT="./artifact.sh"
+ASSEMBLER=$SCRIPT_DIR"/assembler.pl"
+SCAFFOLDER=$SCRIPT_DIR"/scaffolder.pl"
+GAPCLOSER=$SCRIPT_DIR"/gapcloser.pl"
+REPORTER=$TOOLS_DIR"/reporter/dist/reporter.jar"
+SEQ_INFO=$TOOLS_DIR"/sequence_info"
 
 # Copy tools to the dist directory
-cp $ARTIFACT $DIST_DIR
-cp $ASSEMBLER $DIST_DIR
-cp $SCAFFOLDER $DIST_DIR
-cp $GAPCLOSER $DIST_DIR
-cp $REPORTER $DIST_DIR
+cp $ARTIFACT $BIN_DIR
+cp $ASSEMBLER $BIN_DIR
+cp $SCAFFOLDER $BIN_DIR
+cp $GAPCLOSER $BIN_DIR
+cp $REPORTER $BIN_DIR
+cp $SEQ_INFO $BIN_DIR
