1c224dcf0f99567405f75fd5cc600fc749724372
maplesod <daniel.mapleson@tgac.ac.uk>
2012-10-04 14:08:13 +0100

    Added basic functionality to R script, that merges the two tables (raw and quality trimmed) together

--- scripts/assembly_stats_decider.R
+++ scripts/assembly_stats_decider.R
@@ -1,42 +1,30 @@
+# This script processes the tables produced from teh raw and quality trimmed datasets and then determines the best assembly to use for the subsequent scaffolding process
+
+# Get command like arguments.  Expects 4:
+# 1 - raw statistics file
+# 2 - quality trimmed statistics file
+# 3 - approximate genome size
+# 4 - output directory
+
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
 raw_stats <- args[1]
-qt_stats <= args[2]
-
-raw <- read.table(raw_stats, header = TRUE, sep="|", quote = "");
-qt <- read.table(qt_stats, header = TRUE, sep="|", quote = "");
-
-# Separate the file column (keeping this column makes the maths harder)
-raw_file <- raw[,c('kmer','file')]
-qt_file <- qt[,c('kmer','file')]
-
-raw$file <- NULL
-qt$file <- NULL
+qt_stats <- args[2]
+approx_genome_size <- args[3]
+output_dir <- args[4]
 
 
+# Load statistics into data frames
+
+raw <- read.table(raw_stats, header = TRUE, sep="|", quote = "")
+qt <- read.table(qt_stats, header = TRUE, sep="|", quote = "")
 
 
-# Get individual features
-
-raw_nbcontigs <- raw[,c('kmer','nbcontigs')]
-raw_total <- raw[,c('kmer','total')]
-raw_minlen <- raw[,c('kmer','minlen')]
-raw_maxlen <- raw[,c('kmer','maxlen')]
-raw_avglen <- raw[,c('kmer','avglen')]
-raw_n50 <- raw[,c('kmer','n50')]
-
-
-qt_nbcontigs <- qt[,c('kmer','nbcontigs')]
-qt_total <- qt[,c('kmer','total')]
-qt_minlen <- qt[,c('kmer','minlen')]
-qt_maxlen <- qt[,c('kmer','maxlen')]
-qt_avglen <- qt[,c('kmer','avglen')]
-qt_n50 <- qt[,c('kmer','n50')]
-
-
-
-
+# Merge the tables into 1
+raw$dataset <- "raw"
+qt$dataset <- "qt"
+merged <- merge(raw, qt, all = TRUE)
 
 
 
