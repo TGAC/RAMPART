3110dfd6b75af59d835d245ae96c98bbc02b4b69
maplesod <daniel.mapleson@tgac.ac.uk>
2012-10-08 17:04:25 +0100

    Made assembler use strict grammar.

--- scripts/assembler.pl
+++ scripts/assembler.pl
@@ -1,43 +1,44 @@
 #!/usr/bin/perl
 
+use strict;
+
 use Getopt::Long;
 use Pod::Usage;
 use File::Basename;
 use Cwd;
 
-my %args;
 
 # Now
-($sec,$min,$hr,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-$NOW = $year . $mon . $mday . "_" . $hr . $min . $sec;
+my ($sec,$min,$hr,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+my $NOW = $year . $mon . $mday . "_" . $hr . $min . $sec;
 
 # Assembler constants
-$A_ABYSS = "abyss";
-$DEF_ASSEMBLER = $A_ABYSS;
+my $A_ABYSS = "abyss";
+my $DEF_ASSEMBLER = $A_ABYSS;
 
 # Kmer constants
-$KMER_MIN = 11;
-$KMER_MAX = 95;
-$DEF_KMER_MIN = 41;
-$DEF_KMER_MAX = 95;
+my $KMER_MIN = 11;
+my $KMER_MAX = 125;
+my $DEF_KMER_MIN = 41;
+my $DEF_KMER_MAX = 95;
 
 # Threads constants
-$DEF_THREADS=8;
+my $DEF_THREADS=8;
 
 # Mem constants
-$DEF_MEM=60;
-$MIN_MEM=5;
+my $DEF_MEM=60;
+my $MIN_MEM=5;
 
 # Project constants
-$DEF_PROJECT_NAME = "AssemblerMultiKmer_" . $NOW;
-$JOB_PREFIX = $ENV{'USER'} . "-assembler-";
+my $DEF_PROJECT_NAME = "AssemblerMultiKmer_" . $NOW;
+my $DEF_JOB_PREFIX = $ENV{'USER'} . "-assembler-";
 
 # Queueing system constants
-$SUBMIT = "bsub";
+my $SUBMIT = "bsub";
 
 # Other constants
-$QUOTE = "\"";
-$PWD = getcwd;
+my $QUOTE = "\"";
+my $PWD = getcwd;
 
 
 # Assign any command line options to variables
@@ -75,8 +76,8 @@ pod2usage( -verbose => 2 ) if $opt{man};
 
 
 # Get input files
-@in_files = @ARGV;
-$input_files = join " ", @in_files;
+my @in_files = @ARGV;
+my $input_files = join " ", @in_files;
 
 print "\n";
 print "Command line arguments gathered\n\n" if $opt{verbose};
@@ -84,27 +85,27 @@ print "Command line arguments gathered\n\n" if $opt{verbose};
 
 # Argument Validation
 
-die "Error: No input files specified\n\n" . $USAGE unless @in_files;
+die "Error: No input files specified\n\n" unless @in_files;
 foreach(@in_files) {
-        die "Error: Input file does not exist: " . $_ . "\n\n" . $USAGE unless (-e $_);
+        die "Error: Input file does not exist: " . $_ . "\n\n" unless (-e $_);
 }
 
-die "Error: No output directory specified\n\n" . $USAGE unless $opt{output};
+die "Error: No output directory specified\n\n" unless $opt{output};
 
-die "Error: K-mer limits must be >= " . $KMER_MIN . "nt\n\n" . $USAGE unless ($opt{kmin} >= $KMER_MIN && $opt{kmax} >= $KMER_MIN);
-die "Error: K-mer limits must be <= " . $KMER_MAX . "nt\n\n" . $USAGE unless ($opt{kmin} <= $KMER_MAX && $opt{kmax} <= $KMER_MAX);
-die "Error: Min K-mer value must be <= Max K-mer value\n\n" . $USAGE unless ($opt{kmin} <= $opt{kmax});
-die "Error: K-mer min and K-mer max both must end with a '1' or a '5'.  e.g. 41 or 95.\n\n" . $USAGE unless (validKmer($opt{kmin}) && validKmer($opt{kmax}));
+die "Error: K-mer limits must be >= " . $KMER_MIN . "nt\n\n" unless ($opt{kmin} >= $KMER_MIN && $opt{kmax} >= $KMER_MIN);
+die "Error: K-mer limits must be <= " . $KMER_MAX . "nt\n\n" unless ($opt{kmin} <= $KMER_MAX && $opt{kmax} <= $KMER_MAX);
+die "Error: Min K-mer value must be <= Max K-mer value\n\n" unless ($opt{kmin} <= $opt{kmax});
+die "Error: K-mer min and K-mer max both must end with a '1' or a '5'.  e.g. 41 or 95.\n\n" unless (validKmer($opt{kmin}) && validKmer($opt{kmax}));
 
-die "Error: Invalid assembler requested.  Known assemblers are: 'abyss'.\n\n" . $USAGE unless ($opt{assembler} eq "abyss");
+die "Error: Invalid assembler requested.  Known assemblers are: 'abyss'.\n\n" unless ($opt{assembler} eq "abyss");
 
-die "Error: Invalid number of cores requested.  Must request at least 1 core per assembly.\n\n" .$USAGE unless ($opt{threads} >= 1);
+die "Error: Invalid number of cores requested.  Must request at least 1 core per assembly.\n\n" unless ($opt{threads} >= 1);
 
-die "Error: Invalid memory setting.  Must request at least " . $MIN_MEM . "GB.\n\n" . $USAGE unless ($opt{memory} >= $MIN_MEM);
+die "Error: Invalid memory setting.  Must request at least " . $MIN_MEM . "GB.\n\n" unless ($opt{memory} >= $MIN_MEM);
 
 
 print "Validated arguments\n\n" if $opt{verbose};
-print "Input files: " . $in_files . "\n" if $opt{verbose};
+print "Input files: " . $input_files . "\n" if $opt{verbose};
 if ($opt{verbose}) {
 	print "Options:\n";
 	foreach (keys %opt) {
@@ -117,38 +118,39 @@ if ($opt{verbose}) {
 
 # Assembly job loop
 
-$mem_mb = $opt{memory} * 1000;
-$j = 0;
-for($i=$opt{kmin}; $i<=$opt{kmax};) {
+my $mem_mb = $opt{memory} * 1000;
+my $j = 0;
+for(my $i=$opt{kmin}; $i<=$opt{kmax};) {
 
-	$i_dir = $opt{output} . "/" . $i;
-	$job_name = $opt{job_prefix} . $i;
-	$job_arg = "-J " . $job_name;
-	$project_arg = "-P " . $opt{project};
-	$queue_arg = "-q production";
-	$openmpi_arg = "-a openmpi";
-	$rusage_arg = "-R rusage[mem=" . $mem_mb . "] space[ptile=" . $opt{threads} . "]";
-	$threads_arg = "-n 8";
-	$bsub_args= $job_arg . " " . $project_arg . " " . $queue_arg . " " . $openmpi_arg . " " . $rusage_arg . " " . $threads_arg;
+	my $i_dir = $opt{output} . "/" . $i;
+	my $job_name = $opt{job_prefix} . $i;
+	my $job_arg = "-J " . $job_name;
+	my $project_arg = "-P " . $opt{project};
+	my $queue_arg = "-q production";
+	my $openmpi_arg = "-a openmpi";
+	my $rusage_arg = "-R rusage[mem=" . $mem_mb . "] space[ptile=" . $opt{threads} . "]";
+	my $threads_arg = "-n 8";
+	my $bsub_args= $job_arg . " " . $project_arg . " " . $queue_arg . " " . $openmpi_arg . " " . $rusage_arg . " " . $threads_arg;
+	my $cmd_line;
 	
 	if ($opt{assembler} eq $A_ABYSS) {
-		$abyss_bin = "abyss-pe";
-		$abyss_core_args = "n=10 mpirun=mpirun.lsf";
-		$abyss_threads = "np=" . $opt{threads};
-		$abyss_kmer = "k=" . $i;
-		$abyss_name = "name=Abyss-mpi-k" . $i;
-		$abyss_in = "in='" . $input_files . "'";
-		$cmd = $abyss_bin . " " . $abyss_threads . " " . $abyss_core_args . " " . $abyss_kmer . " " . $abyss_name . " " . $abyss_in;
+		my $abyss_bin = "abyss-pe";
+		my $abyss_core_args = "n=10 mpirun=mpirun.lsf";
+		my $abyss_threads = "np=" . $opt{threads};
+		my $abyss_kmer = "k=" . $i;
+		my $abyss_name = "name=Abyss-mpi-k" . $i;
+		my $abyss_in = "in='" . $input_files . "'";
+		$cmd_line = $abyss_bin . " " . $abyss_threads . " " . $abyss_core_args . " " . $abyss_kmer . " " . $abyss_name . " " . $abyss_in;
 	}
 	else {
-		die "Error: Invalid assembler requested.  Also, the script should not have got this far!!!.\n\n" . $USAGE;
+		die "Error: Invalid assembler requested.  Also, the script should not have got this far!!!.\n\n";
 	}
 
 	system("mkdir", $i_dir) unless (-e $i_dir);
   	chdir $i_dir;
 
-	print "Executing on cluster: " . $bsub . " " . $bsub_args . " " . $QUOTE . $cmd . $QUOTE . "\n\n" if ($opt{verbose});
-	system($SUBMIT, $job_arg, $project_arg, $queue_arg, $openmpi_arg, $rusage_arg, $threads_arg, $cmd) unless ($opt{simulate});
+	print "Executing on cluster: " . $SUBMIT . " " . $bsub_args . " " . $QUOTE . $cmd_line . $QUOTE . "\n\n" if ($opt{verbose});
+	system($SUBMIT, $job_arg, $project_arg, $queue_arg, $openmpi_arg, $rusage_arg, $threads_arg, $cmd_line) unless ($opt{simulate});
 
 	if ($j % 2) {
 		$i += 6;
@@ -162,7 +164,7 @@ for($i=$opt{kmin}; $i<=$opt{kmax};) {
 
 # Change back to the original directory before exiting.
 
-chdir $pwd;
+chdir $PWD;
 
 
 # Script finished successfully... but the jobs 
@@ -172,10 +174,10 @@ exit 0;
 
 
 sub validKmer {
-	$val_in = $_[0];
+	my $val_in = $_[0];
 
-	$mod1 = ($val_in - 1) % 10;
-	$mod2 = ($val_in - 5) % 10;
+	my $mod1 = ($val_in - 1) % 10;
+	my $mod2 = ($val_in - 5) % 10;
 
 	if ($mod1 == 0 || $mod2 == 0 ) {
 		return 1;
