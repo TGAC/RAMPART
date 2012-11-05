72a08b80578639d04f2724b939a76946e8d02851
maplesod <daniel.mapleson@tgac.ac.uk>
2012-11-05 13:38:44 +0000

    Rebuilt build.sh
    Added the ReportGenerator tool to rampart

--- build.sh
+++ build.sh
@@ -7,27 +7,12 @@ TEMPLATE_DIR="./templates"
 BIN_DIR="./bin"
 
 # Script / Tool file names
-ARTIFACT="./artifact.sh"
-ASSEMBLER=$SCRIPT_DIR"/assembler.pl"
-A_ANALYSER=$SCRIPT_DIR"/assembly_analyser.pl"
-AS_GATHERER=$SCRIPT_DIR"/assembly_stats_gatherer.pl"
-AS_FORMATTER=$SCRIPT_DIR"/assembly_stats_formatter.pl"
-AS_PLOTTER=$SCRIPT_DIR"/assembly_stats_plotter.R"
-AS_DECIDER=$SCRIPT_DIR"/assembly_stats_decider.R"
-SCAFFOLDER=$SCRIPT_DIR"/scaffolder.pl"
-GAPCLOSER=$SCRIPT_DIR"/gapcloser.pl"
-REPORTER=$TOOLS_DIR"/reporter/dist/reporter.jar"
+REPORTER=$TOOLS_DIR"/ReportGenerator*.jar"
 SEQ_INFO=$TOOLS_DIR"/sequence_info"
 
 # Copy tools to the dist directory
-cp $ARTIFACT $BIN_DIR
-cp $ASSEMBLER $BIN_DIR
-cp $A_ANALYSER $BIN_DIR
-cp $AS_GATHERER $BIN_DIR
-cp $AS_FORMATTER $BIN_DIR
-cp $AS_PLOTTER $BIN_DIR
-cp $AS_DECIDER $BIN_DIR
-cp $SCAFFOLDER $BIN_DIR
-cp $GAPCLOSER $BIN_DIR
+cp *.pl $BIN_DIR
+cp *.pm $BIN_DIR
+cp *.R $BIN_DIR
 cp $REPORTER $BIN_DIR
 cp $SEQ_INFO $BIN_DIR
